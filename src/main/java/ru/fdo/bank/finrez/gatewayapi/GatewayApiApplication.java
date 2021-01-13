6d8c169b48746eccd0815e304e5dddb00733d192
Dmitrii Fokin <fokinds1@gmail.com>
2021-01-13 13:11:25 +0300

    added @EnableZuulProxy @EnableEurekaClient

--- src/main/java/ru/fdo/bank/finrez/gatewayapi/GatewayApiApplication.java
+++ src/main/java/ru/fdo/bank/finrez/gatewayapi/GatewayApiApplication.java
@@ -2,9 +2,12 @@ package ru.fdo.bank.finrez.gatewayapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-
+@EnableZuulProxy
+@EnableEurekaClient
 public class GatewayApiApplication {
 
     public static void main(String[] args) {
